aa5dc0961b4fe130d36e5fd2ef47048bb8f015db
isiratasmika257 <108164278+isiratasmika257@users.noreply.github.com>
2022-06-26 11:53:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,16 +19,16 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94760040313']
+global.premium = ['94760040313']
+global.ownernomer = '94760040313'
+global.ownername = '❤️‍🔥➳᳆⃞⃚𝙺𝚒𝚗𝚐 𝙱𝚕𝚊𝚌𝚔ˢⁿⁱᵖᵉʳහ්᭄'
+global.botname = '➳᳆⃞⃚𝙺𝚒𝚗𝚐 𝙱𝚕𝚊𝚌𝚔ˢⁿⁱᵖᵉʳහ්᭄'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.myweb = 
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
